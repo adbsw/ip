31db693e82021b5145dd31c96ce0ab002e2ab358
ab <179715666+adbsw@users.noreply.github.com>
2026-01-31 13:44:01 +0800

    Rename Duke.java to Green.java, add level-0 into Green.java

---
+++ src/main/java/Green.java
@@ -0,0 +1,24 @@
+/*public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}*/
+
+public class Green {
+    public static void main(String[] args) {
+        String textBlock = """
+                ---------------------------------------------
+                Hello! I'm Green.
+                What can I do for you?
+                ---------------------------------------------
+                Bye. Hope to see you again soon!
+                ---------------------------------------------
+                """;
+        System.out.println(textBlock);
+    }
+}
