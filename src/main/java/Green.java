a684f85f8f90a1e9caaf6a549151fb2b16ce0e7e
ab <179715666+adbsw@users.noreply.github.com>
2026-02-02 11:48:49 +0800

    Add Level-1: Echo

--- src/main/java/Green.java
+++ src/main/java/Green.java
@@ -8,17 +8,32 @@
         System.out.println("Hello from\n" + logo);
     }
 }*/
+import java.util.Scanner;
 
 public class Green {
     public static void main(String[] args) {
-        String textBlock = """
-                ---------------------------------------------
-                Hello! I'm Green.
-                What can I do for you?
-                ---------------------------------------------
-                Bye. Hope to see you again soon!
-                ---------------------------------------------
+        String line;
+        String lineDivider = "-----------------------------------------------\n";
+        String byeCommand = "bye";
+        Scanner in = new Scanner(System.in);
+        System.out.println(lineDivider + "Hi! I'm Green.\nWhat can I do for you today?\n" + lineDivider);
+        while (true) {
+            line = in.nextLine();
+            if (line.equals(byeCommand)) {
+                System.out.println(lineDivider + "Goodbye! Hope to see you again!\n" + lineDivider);
+                break;
+            }
+            System.out.println(lineDivider + line + "\n" + lineDivider);
+        }
+
+        /*String textBlock = """
+                -----------------------------------------------
+                Hi! I'm Green.
+                What can I do for you today?
+                -----------------------------------------------
+                Goodbye! Hope to see you again!
+                -----------------------------------------------
                 """;
-        System.out.println(textBlock);
+        System.out.println(textBlock);*/
     }
 }
