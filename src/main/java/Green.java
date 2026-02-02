6fb4bf73bcf17abcdbec8359ad0690f7f5391c0e
ab <179715666+adbsw@users.noreply.github.com>
2026-02-02 15:23:55 +0800

    Add ability to store input text, display them when requested

--- src/main/java/Green.java
+++ src/main/java/Green.java
@@ -9,12 +9,16 @@
     }
 }*/
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Green {
     public static void main(String[] args) {
         String line;
         String lineDivider = "-----------------------------------------------\n";
         String byeCommand = "bye";
+        String listCommand = "list";
+        String[] list = new String[100];
+        int listIndex = 0;
         Scanner in = new Scanner(System.in);
         System.out.println(lineDivider + "Hi! I'm Green.\nWhat can I do for you today?\n" + lineDivider);
         while (true) {
@@ -22,8 +26,19 @@ public class Green {
             if (line.equals(byeCommand)) {
                 System.out.println(lineDivider + "Goodbye! Hope to see you again!\n" + lineDivider);
                 break;
+            } else if (line.equals(listCommand)) {
+                String[] listDisplay = Arrays.copyOf(list, listIndex);
+                System.out.print(lineDivider);
+                for (int i = 0; i < listIndex; i++) {
+                    System.out.println(i + 1 + ". " + listDisplay[i]);
+                }
+                System.out.println(lineDivider);
+                //System.out.println(lineDivider + Arrays.toString(listDisplay) + "\n" + lineDivider);
+            } else {
+                list[listIndex] = line;
+                listIndex++;
+                System.out.println(lineDivider + "added: " + line + "\n" + lineDivider);
             }
-            System.out.println(lineDivider + line + "\n" + lineDivider);
         }
 
         /*String textBlock = """
