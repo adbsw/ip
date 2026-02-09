a7021b07012445fafde4273537d98e0152a91b3d
adbsw <179715666+adbsw@users.noreply.github.com>
2026-02-09 11:19:14 +0800

    Tweak code to comply with coding standard

--- src/main/java/Green.java
+++ src/main/java/Green.java
@@ -1,37 +1,26 @@
-/*public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
-}*/
 import java.util.Scanner;
-// import java.util.Arrays;
 
 public class Green {
     private static final int ARRAY_SIZE = 100;
-    private static final Task[] tasks = new Task[ARRAY_SIZE];
+    private static Task[] tasks = new Task[ARRAY_SIZE];
     private static int taskIndex = 0;
-    private static final String lineDivider = "-----------------------------------------------\n";
+    private static final String LINE_DIVIDER = "-----------------------------------------------\n";
 
     public static void addTask(Task newTask) {
         if (taskIndex < tasks.length && !isInList(newTask)) {
             tasks[taskIndex] = newTask;
             taskIndex++;
-            System.out.println(lineDivider +
+            System.out.println(LINE_DIVIDER +
                     "Task '" + newTask.getDescription() + "' has been added."
-                    + System.lineSeparator() + lineDivider);
+                    + System.lineSeparator() + LINE_DIVIDER);
         } else if (isInList(newTask)) {
-            System.out.println(lineDivider +
+            System.out.println(LINE_DIVIDER +
                     "Task '" + newTask.getDescription() + "' is already in the list."
-                    + System.lineSeparator() + lineDivider);
+                    + System.lineSeparator() + LINE_DIVIDER);
         } else {
-            System.out.println(lineDivider +
+            System.out.println(LINE_DIVIDER +
                     "Unable to add task. List is full."
-                    + System.lineSeparator() + lineDivider);
+                    + System.lineSeparator() + LINE_DIVIDER);
         }
     }
 
@@ -44,46 +33,55 @@ public class Green {
         return false;
     }
 
-    public static void markTask(Task t, boolean status) {
+    public static void changeTaskStatus(Task t, boolean status) {
         for (int i = 0; i < taskIndex; i++) {
             if (tasks[i].getDescription().equals(t.getDescription())) {
                 tasks[i].setDone(status);
-                System.out.println(lineDivider);
-                System.out.println(lineDivider +
-                        "Task '" + t.getDescription() + "' has been" + (status ? " marked done." : " unmarked.")
-                        + System.lineSeparator() + lineDivider);
+                System.out.println(LINE_DIVIDER +
+                        "Task '" + t.getDescription() + "' has been"
+                        + (status ? " marked done." : " unmarked.")
+                        + System.lineSeparator() + LINE_DIVIDER);
                 return;
             }
         }
-        System.out.println(lineDivider +
+        System.out.println(LINE_DIVIDER +
                 "Task '" + t.getDescription() + "' is not in the list. Would you like to add it?"
-                + System.lineSeparator() + lineDivider);
+                + System.lineSeparator() + LINE_DIVIDER);
     }
 
     public static void showList() {
-        System.out.println(lineDivider + "Here is the list of things:");
+        System.out.println(LINE_DIVIDER + "Here is the list of things:");
         for (int i = 0; i < taskIndex; i++) {
-            System.out.println("[" + tasks[i].getStatusIcon() + "] " + (i + 1) + ". " + tasks[i].getDescription());
+            System.out.println("[" + tasks[i].getStatusIcon() + "] "
+                    + (i + 1) + ". " + tasks[i].getDescription());
         }
-        System.out.println(lineDivider);
+        System.out.println(LINE_DIVIDER);
     }
 
     public static void main(String[] args) {
         String input;
         Scanner in = new Scanner(System.in);
-        System.out.println("Greetings");
+        System.out.println(LINE_DIVIDER + "Greetings from");
+        String logo =
+                """
+                     _____     _____    ____   ____   _    _
+                    |  _ _|   |  _  \\  |  __| |  __| | \\  | |
+                    | |  _ _  | |_| /  |  __| |  __| | |\\ | |
+                    | |_| | | | | \\ \\  |  __| |  __| | | \\| |
+                    |____/ \\_\\\\_|  \\_\\ \\____\\ \\____\\ \\_|  \\_|
+                """;
+        System.out.println(logo);
         System.out.println(
-                lineDivider +
-                        """
-                                Hi! I'm Green. What can I do for you today?
-                                
-                                    To add a new task, enter 'add [task name]' .
-                                    To mark a task as done, enter 'mark [task name]'.
-                                    To unmark a task, enter 'unmark [task name]'.
-                                    To show the current to-do list, enter 'list'.
-                                    Type 'bye' to leave.
-                                """
-                        + lineDivider);
+                """
+                 What would you like to do today?
+                 
+                         To add a new task, enter 'add [task name]'.
+                         To mark a task as done, enter 'mark [task name]'.
+                         To unmark a task, enter 'unmark [task name]'.
+                         To show the current to-do list, enter 'list'.
+                         Type 'bye' to leave.
+                 """
+                 + LINE_DIVIDER);
 
         boolean byeFlag = false;
         while (!byeFlag) {
@@ -99,29 +97,29 @@ public class Green {
             switch (command) {
             case "add":
                 if (words.length < 2) {
-                    System.out.println(lineDivider +
-                            "Missing task entry. What would you like to add?"
-                            + System.lineSeparator() + lineDivider);
+                    System.out.println(LINE_DIVIDER
+                            + "Missing task entry. What would you like to add?"
+                            + System.lineSeparator() + LINE_DIVIDER);
                 } else {
                     addTask(task);
                 }
                 break;
             case "mark":
                 if (words.length < 2) {
-                    System.out.println(lineDivider +
-                            "Missing task entry. What would you like to mark?"
-                            + System.lineSeparator() + lineDivider);
+                    System.out.println(LINE_DIVIDER
+                            + "Missing task entry. What would you like to mark?"
+                            + System.lineSeparator() + LINE_DIVIDER);
                 } else {
-                    markTask(task, true);
+                    changeTaskStatus(task, true);
                 }
                 break;
             case "unmark":
                 if (words.length < 2) {
-                    System.out.println(lineDivider +
-                            "Missing task entry. What would you like to unmark?"
-                            + System.lineSeparator() + lineDivider);
+                    System.out.println(LINE_DIVIDER
+                            + "Missing task entry. What would you like to unmark?"
+                            + System.lineSeparator() + LINE_DIVIDER);
                 } else {
-                    markTask(task, false);
+                    changeTaskStatus(task, false);
                 }
                 break;
             case "list":
@@ -129,8 +127,9 @@ public class Green {
                 break;
             case "bye":
                 byeFlag = true;
-                System.out.println(lineDivider + "Goodbye, have a nice day"
-                        + System.lineSeparator() + lineDivider);
+                System.out.println(LINE_DIVIDER
+                        + "Goodbye, have a nice day"
+                        + System.lineSeparator() + LINE_DIVIDER);
                 break;
             default:
                 break;
