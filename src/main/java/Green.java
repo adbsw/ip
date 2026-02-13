1cf46bef9b8fafa522ec100c184e6ef18dcdc99c
adbsw <179715666+adbsw@users.noreply.github.com>
2026-02-13 19:40:18 +0800

    Modify Green.java and EXPECTED.TXT to improve code readability
    Restructure conditional statements in addTask() method using guard
    clauses to make the happy path prominent.
    
    Simplify complicated expression of if condition in the main method that
    checks if user input is missing a required task entry by calculating
    intermediate boolean values and checking for the final value in
    isMissingTask.
    
    Make other tweaks such as renaming variables with explicit names,
    removing redundant code, and using blank lines to separate groups of
    related code.

--- src/main/java/Green.java
+++ src/main/java/Green.java
@@ -4,25 +4,27 @@ public class Green {
     private static final int ARRAY_SIZE = 100;
     private static Task[] tasks = new Task[ARRAY_SIZE];
     private static int taskCount = 0;
-    private static final String LINE_DIVIDER = "---------------------------------------------------------\n";
+    private static final String LINE_DIVIDER = "---------------------------------------------------------";
+    private static final String OPENING = LINE_DIVIDER + System.lineSeparator();
+    private static final String CLOSING = System.lineSeparator() + LINE_DIVIDER + System.lineSeparator();
 
     /** Adds task to current list */
     public static void addTask(Task newTask) {
-        if (taskCount < tasks.length && !isInList(newTask)) {
-            tasks[taskCount] = newTask;
-            taskCount++;
-            System.out.println(LINE_DIVIDER +
-                    "'" + newTask.getDescription() + "' has been added."
-                    + System.lineSeparator() + LINE_DIVIDER);
-        } else if (isInList(newTask)) {
-            System.out.println(LINE_DIVIDER +
+        if (isInList(newTask)) {
+            System.out.println(OPENING +
                     "Failed to add '" + newTask.getDescription() + "'. It is already in the list."
-                    + System.lineSeparator() + LINE_DIVIDER);
-        } else {
-            System.out.println(LINE_DIVIDER +
-                    "Unable to add task. List is full."
-                    + System.lineSeparator() + LINE_DIVIDER);
+                    + CLOSING);
+            return;
         }
+
+        if (taskCount >= tasks.length) {
+            System.out.println(OPENING + "Unable to add task. List is full." + CLOSING);
+            return;
+        }
+
+        tasks[taskCount] = newTask;
+        taskCount++;
+        System.out.println(OPENING + "'" + newTask.getDescription() + "' has been added." + CLOSING);
     }
 
     /** Returns true if task exists in the current list */
@@ -36,41 +38,40 @@ public class Green {
     }
 
     /** Updates task status accordingly to user's request of 'mark' or 'unmark' */
-    public static void changeTaskStatus(int num, boolean status) {
-        if (num > taskCount|| num == 0) {
-            System.out.println(LINE_DIVIDER + "There is no task numbered " + num + "."
-                    + System.lineSeparator() + LINE_DIVIDER);
+    public static void changeTaskStatus(int listNum, boolean status) {
+        if (listNum > taskCount|| listNum == 0) {
+            System.out.println(OPENING + "There is no task numbered " + listNum + "." + CLOSING);
             return;
         }
 
-        if (tasks[num - 1].isDone() == status) {
-            System.out.println(LINE_DIVIDER
-                    + "'" + num + ". " + tasks[num - 1].getDescription() + "'" + " was previously "
+        int index = listNum - 1;
+        if (tasks[index].isDone() == status) {
+            System.out.println(OPENING
+                    + "'" + listNum + ". " + tasks[index].getDescription() + "'" + " was previously "
                     + (status ? "marked done." : "unmarked.")
-                    + System.lineSeparator() + LINE_DIVIDER);
+                    + CLOSING);
             return;
         }
 
-        tasks[num - 1].setDone(status);
-        System.out.println(LINE_DIVIDER
-                + "'" + num + ". " + tasks[num - 1].getDescription() + "'" + " has been "
+        tasks[index].setDone(status);
+        System.out.println(OPENING
+                + "'" + listNum + ". " + tasks[index].getDescription() + "'" + " has been "
                 + (status ? "marked done." : "unmarked.")
-                + System.lineSeparator() + LINE_DIVIDER);
+                + CLOSING);
     }
 
     /** Prints current list of tasks */
     public static void showList() {
         if (taskCount == 0) {
-            System.out.println(LINE_DIVIDER + "Your list is empty."
-                    + System.lineSeparator() + LINE_DIVIDER);
+            System.out.println(OPENING + "Your list is empty." + CLOSING);
             return;
         }
 
-        System.out.println(LINE_DIVIDER + "Here is the list of things:");
+        System.out.println(OPENING + "Here is the list of things:");
         for (int i = 0; i < taskCount; i++) {
             System.out.println((i + 1) + ". " + tasks[i].toString());
         }
-        System.out.println(LINE_DIVIDER);
+        System.out.println(LINE_DIVIDER + System.lineSeparator());
     }
 
     public static String parseCommand(String[] words) {
@@ -81,22 +82,23 @@ public class Green {
     }
 
     /** Extract and return corresponding details and class of task from user input */
-    public static Task parseInput(String command, String[] words) {
+    public static Task parseTask(String command, String[] words) {
         if (words.length <= 1) {
             return null;
         }
 
         String[] details = words[1].trim().split("/");
         String description = details[0].trim();
+
         switch (command) {
         case "todo":
             return new ToDo(description);
         case "deadline":
-            String by = details[1].substring(("by".length() + 1)).trim();
+            String by = details[1].substring(("by".length())).trim();
             return new Deadline(description, by);
         case "event":
-            String from = details[1].substring(("from".length() + 1)).trim();
-            String to = details[2].substring(("to".length() + 1)).trim();
+            String from = details[1].substring(("from".length())).trim();
+            String to = details[2].substring(("to".length())).trim();
             return new Event(description, from, to);
         default:
             return new Task(description);
@@ -106,7 +108,7 @@ public class Green {
     public static void main(String[] args) {
         String input;
         Scanner in = new Scanner(System.in);
-        System.out.println(LINE_DIVIDER + "Greetings from");
+        System.out.println(OPENING + "Greetings from");
         String logo =
                 """
                      _____     _____    ____   ____   _    _
@@ -122,15 +124,15 @@ public class Green {
                  
                          To add a new todo, deadline  or event, enter:
                          - 'todo [task name]'
-                         - 'deadline [task name] /by [task's deadline]'
-                         - 'event [task name] /from [event start] /to [event end]'
+                         - 'deadline [task name] /by [date/time]'
+                         - 'event [task name] /from [date/time] /to [date/time]'
                  
                          To show the current task list, enter 'list'.
-                         To mark a task as done, enter 'mark [task number]'.
-                         To unmark a task, enter 'unmark [task number]'.
+                         To mark a task as done, enter 'mark [task list number]'.
+                         To unmark a task, enter 'unmark [task list number]'.
                          Enter 'bye' to leave.
                  """
-                 + LINE_DIVIDER);
+                 + CLOSING);
 
         boolean byeFlag = false;
         while (!byeFlag) {
@@ -140,16 +142,18 @@ public class Green {
 
             String command = parseCommand(words);
             if (command == null) {
-                System.out.println(LINE_DIVIDER + "Invalid command entered."
-                        + System.lineSeparator() + LINE_DIVIDER);
+                System.out.println(OPENING + "Invalid command entered." + CLOSING);
                 continue;
             }
 
-            Task task = parseInput(command, words);
-            // if input is missing a required task entry, skip current iteration and proceed to the next
-            if (!(command.equals("list") || command.equals("bye")) && (task == null || words.length < 2)) {
-                System.out.println(LINE_DIVIDER + "Missing task entry."
-                        + System.lineSeparator() + LINE_DIVIDER);
+            Task task = parseTask(command, words);
+
+            boolean isStandAloneCommand = command.equals("list") || command.equals("bye");
+            boolean isSingleWordInput = task == null || words.length < 2;
+            boolean isMissingTask = !isStandAloneCommand && isSingleWordInput;
+
+            if (isMissingTask) {
+                System.out.println(OPENING + "Missing task entry." + CLOSING);
                 continue;
             }
 
@@ -161,20 +165,17 @@ public class Green {
                 break;
             case "mark":
             case "unmark":
-                int index = Integer.parseInt(task.getDescription());
-                changeTaskStatus(index, command.equals("mark"));
+                int listNum = Integer.parseInt(task.getDescription());
+                changeTaskStatus(listNum, command.equals("mark"));
                 break;
             case "list":
                 showList();
                 break;
             case "bye":
                 byeFlag = true;
-                System.out.println(LINE_DIVIDER
-                        + "Goodbye, have a nice day."
-                        + System.lineSeparator() + LINE_DIVIDER);
+                System.out.println(OPENING + "Goodbye, have a nice day." + CLOSING);
                 break;
-            default: System.out.println(LINE_DIVIDER + "Not sure what you mean."
-                    + System.lineSeparator() + LINE_DIVIDER);
+            default: System.out.println(OPENING + "Not sure what you mean." + CLOSING);
             }
         }
     }
